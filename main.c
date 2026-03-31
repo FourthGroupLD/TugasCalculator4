d6be750d1e2f8198862d709ae968373e0b12ca9e
FourthGroupLD <Farrelsatrianos@gmail.com>
2026-03-31 21:18:10 +0700

    Menambahkan 4 pilihan di UI/UX

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 
 int main() {
     printf("=====================================\n");
-    printf("       KALKULATOR KONVERSI SATUAN    \n");
+    printf("    KALKULATOR KEUANGAN MAHASISWA    \n");
     printf("=====================================\n");
-    printf("Pilih Konversi:\n");
+    printf("Pilih Menu Helper:\n");
+    printf("1. Discount Counter\n");
+    printf("2. Split Bill\n");
+    printf("3. Savings Interest\n");
+    printf("4. Allowance Leftover\n");
     return 0;
 }
