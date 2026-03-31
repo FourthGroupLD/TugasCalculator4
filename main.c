47386658fff3e464d708e042908b3d952034005f
sayaka076 <144534005+sayaka076@users.noreply.github.com>
2026-04-01 05:26:06 +0700

    pilihan user

--- main.c
+++ main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 
 int main() {
+    int pilihan;
+
     printf("=====================================\n");
-    printf("       KALKULATOR KONVERSI SATUAN    \n");
+    printf("    KALKULATOR KEUANGAN MAHASISWA    \n");
     printf("=====================================\n");
-    printf("Pilih Konversi:\n");
+    printf("Pilih Menu Helper:\n");
+    printf("1. Discount Counter\n");
+    printf("2. Split Bill\n");
+    printf("3. Savings Interest\n");
+    printf("4. Allowance Leftover\n");
+    printf("=====================================\n");
+    printf("Masukkan pilihan (1-4): ");
+    scanf("%d", &pilihan);
+
+    switch (pilihan) {
+        case 1:
+            printf("\n[Discount Counter]\n");
+            printf("contoh\n");
+            break;
+
+        case 2:
+            printf("\n[Split Bill]\n");
+            printf("contoh\n");
+            break;
+
+        case 3:
+            printf("\n[Savings Interest]\n");
+            printf("contoh\n");
+            break;
+
+        case 4:
+            printf("\n[Allowance Leftover]\n");
+            printf("contoh\n");
+            break;
+
+        default:
+            printf("\nPilihan tidak valid!\n");
+            break;
+    }
+
     return 0;
 }
