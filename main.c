04c5e9de991cb4001e6bf2fcfe52227b42d5af92
FourthGroupLD <Farrelsatrianos@gmail.com>
2026-04-01 15:32:46 +0700

    Menambah warna di tampilan depan

--- main.c
+++ main.c
@@ -72,21 +72,30 @@ void kalkulatorSaku() {
     printf("Sisa uang saku adalah Rp %d\n", sisa);
 }
 
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define RESET "\e[0m"
 
 int main() {
     int pilihan;
 
-    printf("=====================================\n");
-    printf("    KALKULATOR KEUANGAN MAHASISWA    \n");
-    printf("=====================================\n");
-    printf("Pilih Menu Helper:\n");
+    printf(GRN "=====================================\n" RESET);
+    printf(YEL "    KALKULATOR KEUANGAN MAHASISWA    \n" RESET);
+    printf(GRN "=====================================\n" RESET);
+    printf(BLU "Pilih Menu Helper:\n" RESET);
     printf("1. Discount Counter\n");
     printf("2. Split Bill\n");
     printf("3. Savings Interest\n");
     printf("4. Allowance Leftover\n");
     printf("5. Keluar\n");
-    printf("=====================================\n");
-    printf("Masukkan pilihan (1-5): ");
+    printf(GRN "=====================================\n" RESET);
+    printf(BLU "Masukkan pilihan (1-5): " RESET);
     scanf("%d", &pilihan);
 
     switch (pilihan) {
