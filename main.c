24fb177eee7a4e8830655520cc8a612d43aeef6c
FourthGroupLD <Farrelsatrianos@gmail.com>
2026-04-01 16:10:12 +0700

    Shabrina Color

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define RESET "\e[0m"
+
+
 void kalkulatorBunga() {
     float saldoAwal, bunga, totalAkhir;
 
@@ -81,17 +92,6 @@ void kalkulatorSaku(){
     }
 
 }
-
-
-#define BLK "\e[0;30m"
-#define RED "\e[0;31m"
-#define GRN "\e[0;32m"
-#define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-#define WHT "\e[0;37m"
-#define RESET "\e[0m"
 
 int main() {
     int pilihan;
