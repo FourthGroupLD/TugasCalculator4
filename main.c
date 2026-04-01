95b06185ce5f71646f03ca397a377f4038b869b7
mjovanfadillah <mjovanfadillah@gmail.com>
2026-04-01 20:56:07 +0700

    menambahkan fungsi kalkulator diskon belanja

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define WHT "\e[0;37m"
+#define RESET "\e[0m"
+
 void kalkulatorDiskon() {
     float hargaAwal, persentaseDiskon, hargaAkhir, nominalDiskon;
 
-    printf("\n=== Kalkulator Diskon Belanja ===\n");
+    printf(WHT "\n=== Kalkulator Diskon Belanja ===\n" RESET);
 
   
-    printf("Masukkan harga awal barang:Rp.");
+    printf(GRN "Masukkan harga awal barang:Rp." RESET);
     scanf("%f", &hargaAwal);
     
     
-    printf("Masukkan persentase diskon (%%): ");
+    printf(YEL "Masukkan persentase diskon (%%): " RESET);
     scanf("%f", &persentaseDiskon);
 
     
@@ -21,6 +26,6 @@ void kalkulatorDiskon() {
     printf("\n--- Rincian ---\n");
     printf("Harga Awal    : Rp %.2f\n", hargaAwal);
     printf("Potongan      : Rp %.2f\n", nominalDiskon);
-    printf("Harga Akhir yang harus dibayar: Rp %.2f\n", hargaAkhir);
+    printf(GRN "Harga Akhir yang harus dibayar: Rp %.2f\n" RESET, hargaAkhir);
     printf("=================================\n\n");
 }
