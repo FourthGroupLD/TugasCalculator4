d2b54a87e0903ff4be10e8abd18818b69d5ca170
shabrinaputri2409-ops <shabrinaputri2409@gmail.com>
2026-04-01 15:43:14 +0700

    Menambahkan warna untuk format splitbill

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define RESET "\e[om"
+
 void splitBill() {
     float totalTagihan, perOrang;
     int jumlahOrang;
 
-    printf("\n=== Kalkulator Split Bill / Patungan ===\n");
+    printf( CYN "\n=== Kalkulator Split Bill / Patungan ===\n" RESET );
 
-    printf("Masukkan total tagihan makan: Rp ");
+    printf( RED "Masukkan total tagihan makan: Rp " RESET );
     scanf("%f", &totalTagihan);
 
-    printf("Masukkan jumlah orang: ");
+    printf( GRN "Masukkan jumlah orang: " RESET );
     scanf("%d", &jumlahOrang);
 
     if (jumlahOrang > 0) {
